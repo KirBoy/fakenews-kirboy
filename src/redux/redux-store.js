50474de26ff51156734e1799bb698f31c5eed126
KirBoy <kirll.boy@yandex.ru>
2022-02-03 18:39:33 +0300

    fix

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,20 +3,17 @@ import authReducer from "./authReducer";
 import thunk from "redux-thunk";
 import postsReducer from "./postsReducer";
 import userReducer from "./userReducer";
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducers = combineReducers({
     auth: authReducer,
-    posts:postsReducer,
-    user:userReducer,
+    posts: postsReducer,
+    user: userReducer,
 })
 
 
 const store = createStore(
     reducers,
-    composeWithDevTools(
-        applyMiddleware(thunk)
-    )
+    applyMiddleware(thunk)
 );
 window.store = store;
 export default store;
